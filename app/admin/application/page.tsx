de2e9429a51ea202c7efe647bd1147909a81685b
Mohammed <88824957+m7medVision@users.noreply.github.com>
2024-12-16 13:38:18 +0400

    feat: add initial structure for application and jobs pages

---
+++ app/admin/application/page.tsx
@@ -0,0 +1,9 @@
+export default async function Home() {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 w-[80%] mx-auto py-8">
+      {
+        // TODO: application component here
+      }
+    </div>
+  );
+}
