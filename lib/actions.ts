93972e19b6665a66f661ef1b38667d977418fdc2
Mohammed <88824957+m7medVision@users.noreply.github.com>
2024-12-16 17:20:17 +0400

    feat: add getUserFromToken function for JWT verification

--- lib/actions.ts
+++ lib/actions.ts
@@ -2,6 +2,11 @@
 
 import { jobs } from "@/db/schema";
 import { db } from "./db";
+import { NextRequest } from "next/server";
+import jwt from "jsonwebtoken";
+import { users } from "@/db/schema"; // Your schema
+import { eq } from "drizzle-orm";
+import { cookies } from "next/headers";
 
 export const GetJobs = async () => {
     try {
@@ -10,5 +15,37 @@ export const GetJobs = async () => {
     } catch (error) {
         console.error(error);
         return [];
+    }
+}
+
+export const getUserFromToken = async () => {
+    const cookies_ = await cookies();
+    const token = cookies_.get("token")?.value;
+    if (!token) {
+        return { error: "Unauthorized. Token missing", status: 401 };
+    }
+    try {
+        try {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET!);
+            const userId = (decoded as { userId: number }).userId;
+
+            const user = await db
+                .select()
+                .from(users)
+                .where(eq(users.id, userId))
+                .limit(1);
+
+            if (!user || user.length === 0) {
+                return { error: "User not found", status: 404 };
+            }
+
+            return { id: user[0].id, email: user[0].email };
+        } catch (error) {
+            console.error("Error verifying JWT:", error);
+            return { error: "Invalid or expired token", status: 401 }; // Unauthorized due to invalid token
+        }
+    } catch (error) {
+        console.error("Error in getUserFromToken:", error);
+        return { error: "Internal Server Error", status: 500 };
     }
 }
