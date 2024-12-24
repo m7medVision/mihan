0ec8b0d262f67f7575b4bcf7cacaafe4593f1139
Mohammed <88824957+m7medVision@users.noreply.github.com>
2024-12-24 17:35:09 +0400

    fix: adjust navbar width for better responsiveness

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -20,7 +20,7 @@ const Navbar = async () => {
   const isLoggedIn = !userData.error
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 flex justify-center">
-      <div className="container flex h-14 items-center justify-between px-4">
+      <div className="w-screen mx-10 flex h-14 items-center justify-between px-4">
         <div className="mr-4 hidden md:flex">
           <Link href="/" className="mr-6 flex items-center space-x-2">
             <Briefcase className="h-6 w-6" />
